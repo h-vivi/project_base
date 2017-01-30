8085ef2953b6e12878a537fa7b7d2bf9a829e40e
bq-hentai <bq_hentai@yahoo.com>
2017-01-30 11:13:03 +0800

    增加简单的 require 配置 & l vivi

---
+++ doc/js/main.js
@@ -0,0 +1,17 @@
+/*!
+ * entrance of require
+ * now only for login page
+ * ranran[vivi] & bq-hentai | 01/30/2017
+ */
+
+require.config({
+    baseUrl: './',
+
+    paths: {
+
+    }
+});
+
+require([ ], function() {
+    alert('vivi love');
+});
