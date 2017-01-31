7dcdcca2649707ee9b5c5bf488102d9d022595d4
bq-hentai27 <bq_hentai@yahoo.com>
2017-01-31 21:14:25 +0800

    增加 login controller

--- doc/js/main.js
+++ doc/js/main.js
@@ -17,10 +17,12 @@ var href = location.href;
 // get controller name
 var controllerName = href.slice(href.lastIndexOf('/') + 1, href.lastIndexOf('.'));
 // get controller path
-var controllerPath = '../js/controller/' + controllerName + '-ctrl.js';
+var controllerPath = '../js/controller/' + controllerName + '.js';
 
 require([ 'zepto' ], function(zepto) {
     require([ controllerPath ], function(ctrl) {
+        console.log('controller is', ctrl);
+        console.log('controller exec result is', ctrl());
         setTimeout(function() {
             $('body').html('<span>我要开始咯，虽然不知道要开始干嘛(◍ ´꒳` ◍)<span>');
         });
