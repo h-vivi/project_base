3d20b7f7ed89226590200767e262e42c2ca3198c
王子薇 <ziwei@wangziweideMacBook-Pro.local>
2017-02-08 21:52:56 +0800

    又忘了

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var path$ = require('path');
+var gulp$ = require('gulp');
+
+gulp$.task('default', function(){
+    console.log('I love ranran.');
+});
+
+gulp$.task('love', function() {
+    console.log('Yes, love~~');
+});
