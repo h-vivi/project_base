aae58f700a11d3e7cfd7c35b459d843dd9a1c7a1
bq-hentai27 <bq_hentai@yahoo.com>
2017-02-09 00:03:20 +0800

    love

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var path$ = require('path');
 var gulp$ = require('gulp');
 var rjs$ = require('gulp-requirejs');
 var sourcemaps$ = require('gulp-sourcemaps');
+var gulpFileInclude$ = require('gulp-file-include');
 
 gulp$.task('default', function(){
     console.log('I love ranran.');
@@ -11,15 +12,46 @@ gulp$.task('love', function() {
     console.log('Yes, love~~');
 });
 
-gulp$.task('requirejsBuild', function(){
+gulp$.task('rjs-build', function(){
     return rjs$({
-        baseUrl: 'src/js/main.js',
-        out: 'index.bundle.js',
-        shim: {
+        baseUrl: './src',
+        name: 'js/main',
 
+        optimize: "uglify",
+        uglify2: {
+            output: {
+                beautify: false
+            },
+            compress: {
+                sequences: false,
+                global_defs: {
+                    DEBUG: false
+                }
+            },
+            preserveLicenseComments: true,
+            warnings: true,
+            mangle: true
+        },
+
+        optimizeCss: 'standard',
+
+        removeCombined: true,
+
+        out: 'index.bundle.js',
+        generateSourceMaps: true,
+
+        paths: {
+            zepto: 'js/libs/zepto',
         }
     })
-    .pipe(sourcemaps$.init({loadMaps: true}))
+    .pipe(sourcemaps$.init({ loadMaps: true }))
     .pipe(sourcemaps$.write())
-    .pipe(gulp$.dest('./deploy/'));
+    .pipe(gulp$.dest('./dist/'));
+});
+
+gulp$.task('include-common-template-files', function() {
+    gulp.src('./html/*.html')
+        .pipe(gulpFileInclude$({
+            prefix: '@@'
+        }));
 });
