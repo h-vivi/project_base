c060eb238a39ab6c13cd8675e618485eccd5884c
王子薇 <ziwei@wangziweideMacBook-Pro.local>
2017-02-08 22:55:42 +0800

    gulprjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var path$ = require('path');
 var gulp$ = require('gulp');
+var rjs$ = require('gulp-requirejs');
+var sourcemaps$ = require('gulp-sourcemaps');
 
 gulp$.task('default', function(){
     console.log('I love ranran.');
@@ -7,4 +9,17 @@ gulp$.task('default', function(){
 
 gulp$.task('love', function() {
     console.log('Yes, love~~');
+});
+
+gulp$.task('requirejsBuild', function(){
+    return rjs$({
+        baseUrl: 'src/js/main.js',
+        out: 'index.bundle.js',
+        shim: {
+
+        }
+    })
+    .pipe(sourcemaps$.init({loadMaps: true}))
+    .pipe(sourcemaps$.write())
+    .pipe(gulp$.dest('./deploy/'));
 });
